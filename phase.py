9631254a2471a54b2085e02114c18a474ddbf01f
igor-alves1 <ig_rodrigues@id.uff.br>
2023-10-26 12:32:18 -0300

    FIX| O jogo possui um final de fato, e não apresenta erro\n FEAT| Adição das funções do especial e das qualidades good e ok

--- phase.py
+++ phase.py
@@ -27,7 +27,9 @@ readToList(f'docs/{music}/{music}_down_file.txt', down_list)
 readToList(f'docs/{music}/{music}_left_file.txt', left_list)
 readToList(f'docs/{music}/{music}_right_file.txt', right_list)
 
+
 arrows_speed = 140
+arrow_tick = 0
 up_i, down_i, left_i, right_i = 0, 0, 0, 0
 
 timer = 0
@@ -51,19 +53,42 @@ checker_left = Sprite("left_checker.png")
 checker_left.set_position(14*screen.width/16, 13*screen.height/16)
 
 image_meter = GameImage("fix_meter.png")
-image_meter.set_position(15*screen.width/16, (screen.height-image_meter.height)/2)
+image_meter.set_position(14*screen.width/16, 2*(screen.height-image_meter.height)/6)
 super_meter = Sprite("meter.png")
 super_meter.set_position(image_meter.x, image_meter.y)
 super_meter.height = 0
 
-every_one_s = 0
-
-def judge_inputs(key_entered, arrows_array):
-    if len(arrows_array)>0 and abs(arrows_array[0].x - checker_up.x) <= 10:
+def judge_inputs(key_entered, arrows_array, super_bar):
+    if len(arrows_array)>0 and abs(arrows_array[0].x-checker_right.x)<5:
+        super_bar.height += 5
         return "perfect"
+    elif len(arrows_array)>0 and abs(arrows_array[0].x-checker_right.x)<15:
+        return "good"
+    elif len(arrows_array)>0 and abs(arrows_array[0].x-checker_right.x)<25:
+        return "ok"
     else:
-        return "miss"         
+        super_bar.height = 0
+        return "miss"
+
+def judge_resposta(resposta):
+    if resposta == "perfect":
+        return 50
+    elif resposta == "good":
+        return 20
+    elif resposta == "ok":
+        return 10
+    return -30
 resposta = ""
+
+score = 0
+
+def special_use(moving_arrows_array):
+    score_special = 50
+    for arrow in moving_arrows_array:
+        moving_arrows_array.remove(arrow)
+        score_special += 50
+
+    return score_special
 
 while True:
     screen.set_background_color((0, 0, 0))
@@ -73,7 +98,8 @@ while True:
     checker_down.draw()
     image_meter.draw()
     super_meter.draw()
-    
+
+    #inicia o countdown quando é apertada a tecla enter
     if keyboard.key_pressed('enter') or start:
         countdown -= screen.delta_time()
         start = True
@@ -82,35 +108,36 @@ while True:
             start = False
             song.play()
             start_timer = True
-    
+            
+    #inicia o timer e exibe o timer e o score na tela
     if start_timer:
         timer += screen.delta_time()
         screen.draw_text(f'{timer:.1f}  {resposta}', screen.width/2, screen.height/8, color=(255,255,255))
-        if timer >= every_one_s+1:
-            every_one_s += 1
-            super_meter.height += 5
+        screen.draw_text(f'{score}', screen.width/2, screen.height/8+20, color=(255,255,255))
 
-    if timer >= (up_list[up_i]-4.9):
+    #faz as setinhas surgirem com delay para chegarem do outro lado na tela no tempo certo
+    if len(up_list)-1 > up_i  and timer >= (up_list[up_i]-4.9):
         new_up = Sprite("UpArrow.png")
         new_up.set_position(-new_up.width, 11*screen.height/16)
         moving_arrows_up.append(new_up)
         up_i += 1
-    if timer >= (down_list[down_i]-4.9):
+    if len(down_list)-1> down_i and timer >= (down_list[down_i]-4.9):
         new_down = Sprite("down_arrow.png")
         new_down.set_position(-new_up.width, 12*screen.height/16)
         moving_arrows_down.append(new_down)
         down_i += 1
-    if timer >= (left_list[left_i]-4.9):
+    if len(left_list)-1>left_i and timer >= (left_list[left_i]-4.9):
         new_left = Sprite("left_arrow.png")
         new_left.set_position(-new_up.width, 13*screen.height/16)
         moving_arrows_left.append(new_left)
         left_i += 1
-    if timer >= (right_list[right_i]-4.9):
+    if len(right_list)-1> right_i and timer >= (right_list[right_i]-4.9):
         new_right = Sprite("right_arrow.png")
         new_right.set_position(-new_up.width, 14*screen.height/16)
         moving_arrows_right.append(new_right)
         right_i += 1
 
+    #desenha setinhas na tela
     for arrow in moving_arrows_up:
         arrow.draw()
         arrow.move_x(arrows_speed*screen.delta_time())
@@ -123,23 +150,56 @@ while True:
     for arrow in moving_arrows_right:
         arrow.draw()
         arrow.move_x(arrows_speed*screen.delta_time())
-
-    if len(moving_arrows_up)> 0 and moving_arrows_up[0].x >= screen.width:
+        
+    #remove as setas fora da tela das listas de setas para movimentar
+    if len(moving_arrows_up) and moving_arrows_left[0].x >= screen.width:
+        resposta = "miss"
         x = moving_arrows_up.pop(0)
-    if len(moving_arrows_down)> 0 and moving_arrows_down[0].x >= screen.width:
+        score -= 30
+    if len(moving_arrows_down) and moving_arrows_down[0].x >= screen.width:
+        resposta = "miss"
         x = moving_arrows_down.pop(0)
-    if len(moving_arrows_left)> 0 and moving_arrows_left[0].x >= screen.width:
+        score -= 30
+    if len(moving_arrows_left) and moving_arrows_left[0].x>= screen.width:
+        resposta = "miss"
         x = moving_arrows_left.pop(0)
-    if len(moving_arrows_right)> 0 and moving_arrows_right[0].x >= screen.width:
+        score -= 30
+    if len(moving_arrows_right) and moving_arrows_right[0].x >= screen.width:
+        resposta = "miss"
         x = moving_arrows_right.pop(0)
+        score -= 30
+    
+    #avalia o timing em que uma tecla foi apertada, contando a pontuação
+    if keyboard.key_pressed("UP") and arrow_tick > 0.3:
+        resposta = judge_inputs('UP', moving_arrows_up, super_meter)
+        score += judge_resposta(resposta)
+        arrow_tick = 0
+        moving_arrows_up.pop(0)
+    elif keyboard.key_pressed("DOWN") and arrow_tick > 0.3:
+        resposta = judge_inputs('DOWN', moving_arrows_down, super_meter)
+        score += judge_resposta(resposta)
+        arrow_tick = 0
+    elif keyboard.key_pressed("LEFT") and arrow_tick > 0.3:
+        resposta = judge_inputs('LEFT', moving_arrows_left, super_meter)
+        score += judge_resposta(resposta)
+        arrow_tick = 0
+    elif keyboard.key_pressed("right") and arrow_tick > 0.3:
+        resposta = judge_inputs('right', moving_arrows_right, super_meter)
+        score += judge_resposta(resposta)
+        arrow_tick = 0
+    else:
+        arrow_tick += screen.delta_time()
 
-    if keyboard.key_pressed("UP"):
-        resposta = judge_inputs('UP', moving_arrows_up)
-    elif keyboard.key_pressed("DOWN"):
-        resposta = judge_inputs('DOWN', moving_arrows_down)
-    elif keyboard.key_pressed("LEFT"):
-        resposta = judge_inputs('LEFT', moving_arrows_left)
-    elif keyboard.key_pressed("right"):
-        resposta = judge_inputs('right', moving_arrows_right)
+    #ação do especial
+    if keyboard.key_pressed("SPACE") and super_meter.height >=128:
+        score += special_use(moving_arrows_up)
+        score += special_use(moving_arrows_down)
+        score += special_use(moving_arrows_right)
+        score += special_use(moving_arrows_left)
+        super_meter.height = 0
+
+    if up_list[-1] < timer:
+        start_timer = False
+        screen.draw_text(f'Finish!', screen.width/2, screen.height/8, color=(255,255,255))
         
     screen.update()
